5e402eb353bcf1a2ae8c7a24875d704ac4cca90f
dreamergirl01 <rahma01twins@gmail.com>
2025-01-30 17:22:25 +0700

    get all data, get by id

--- src/main/java/book_springboot/crud_book_springboot/controller/BookController.java
+++ src/main/java/book_springboot/crud_book_springboot/controller/BookController.java
@@ -5,6 +5,8 @@ import book_springboot.crud_book_springboot.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/book")
 public class BookController {
@@ -21,5 +23,17 @@ public class BookController {
     public BookEntity addNewBook(@RequestBody BookEntity param) {
         bookRepository.save(param);
         return param;
+    }
+
+//    tampilkan semua data
+    @GetMapping(value = "getAllBook")
+    public List<BookEntity> getAllBook() {
+        return bookRepository.findAll();
+    }
+
+//    tampilkan berdasarkan id
+    @GetMapping(value = "getById")
+    public BookEntity getById(@RequestParam(value = "id") Integer id) {
+        return bookRepository.findById(id).get();
     }
 }
